43287e1f114fcdcdeae5c969dae08cb555316ea1
Ascoos Development <ascoos@users.noreply.github.com>
2024-09-29 08:09:34 +0300

    Initial

---
+++ languages/el.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ***********************************************************************************
+ * @ASCOOS-NAME        : ASCOOS CMS 24'                                            *
+ * @ASCOOS-VERSION     : 24.0.0                                                    *
+ * @ASCOOS-CATEGORY    : Block (Frontend and Administrator Side)                   *
+ * @ASCOOS-CREATOR     : Drogidis Christos                                         *
+ * @ASCOOS-SITE        : www.ascoos.com                                            *
+ * @ASCOOS-LICENSE     : [Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html  *
+ * @ASCOOS-COPYRIGHT   : Copyright (c) 2007 - 2024, AlexSoft Software.             *
+ ***********************************************************************************
+ *
+ * @package            : Background Selector
+ * @subpackage         : Greek Languages (el)
+ * @source             : /[BLOCKS PATH]/background_selector/languages/el.php
+ * @fileNo             : 
+ * @version            : 1.0.1
+ * @created            : 2012-07-27 20:00:00 UTC+3 
+ * @updated            : 2024-09-29 07:00:00 UTC+3
+ * @author             : Drogidis Christos
+ * @authorSite         : www.alexsoft.gr
+ * @translator	       :
+ * @translatorSite     :
+ * @translatorCreated  :
+ * @translatedUpdated  : 
+*/
+
+defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
+
+class TBlock_background_selector_Language extends TObject
+{
+	public $blockname = "Επιλογή υποβάθρου ιστοσελίδας";
+
+	public $APL_license = "Ascoos General License (AGL)";
+	public $APL_author = "ΔΡΟΓΚΙΔΗΣ ΧΡΗΣΤΟΣ";
+	public $APL_creation = "";
+	public $APL_copyright = "ALEXSOFT ΛΟΓΙΣΜΙΚΟ";
+	public $APL_translator = "";
+	public $APL_translatorEmail = "";
+	public $APL_translatorUrl = "";
+	public $APL_desc = "Με το ένθεμα αυτό, μπορούμε να αλλάξουμε δυναμικά την εικόνα του υποβάθρου της ιστοσελίδας μας, ή ακόμη και μεμονομένων στοιχείων όπως DIVS κλπ.";
+	
+	public $APL_paramgroup_dates_label = "▼ &nbsp; Επιλογές Ημερομηνιών Εφαρμογής Παραμέτρων";
+	public $APL_paramgroup_dates_label_info = "<strong>ΕΠΙΛΟΓΕΣ ΗΜΕΡΟΜΗΝΙΩΝ</strong><br />--------------------------------------<br /><br />Στο πλαίσιο αυτό μπορείτε να επιλέξετε εάν θέλετε η ενότητα αυτή να έχει χρονικά όρια λειτουργίας.";
+	public $APL_sdate_label = "Ημερομηνία έναρξης εμφάνισης υποβάθρου";
+	public $APL_sdate_desc = "<strong>ΗΜΕΡΟΜΗΝΙΑ ΕΝΑΡΞΗΣ</strong><br />--------------------------------------<br /><br />Πατήστε δίπλα στο πλαίσιο εισαγωγής και επιλέξτε από το πλαίσιο ημερομηνίας την ημερομηνία έναρξης λειτουργίας της ενότητας αυτής.<br /><br />Μπορείτε να δώσετε μεταγενέστερη ημερομηνία έναρξης.";
+	public $APL_edate_label = "Ημερομηνία λήξης υποβάθρου";
+	public $APL_edate_desc = "<strong>ΗΜΕΡΟΜΗΝΙΑ ΛΗΞΗΣ</strong><br />--------------------------------------<br /><br />Πατήστε δίπλα στο πλαίσιο εισαγωγής και επιλέξτε από το πλαίσιο ημερομηνίας την ημερομηνία λήξης λειτουργίας της ενότητας αυτής.<br /><br />Συνίσταται η χρήση της επιλογής αυτής για να έχετε στοχευμένες παρουσιάσεις ανά σελίδα ή ημέρα κλπ.";
+
+	public $APL_paramgroup_bg_label = "▼ &nbsp; Επιλογές Εικόνας Υποβάθρου";
+	public $APL_paramgroup_bg_label_info = "<strong>ΕΠΙΛΟΓΕΣ ΕΙΚΟΝΑΣ</strong><br />--------------------------------------<br /><br />Στο πλαίσιο αυτό μπορείτε να επιλέξετε διάφορες παραμέτρους για να διαμορφώσετε την εικόνα υποβάθρου.";
+	public $APL_id_label = "Στοιχείο CSS";
+	public $APL_id_desc = "<strong>ΣΤΟΙΧΕΙΟ CSS</strong><br />--------------------------------------<br /><br />Δώστε εδώ το στοιχείο ή τα στοιχεία CSS που θέλετε να περιέχουν την επιλεγόμενη εικόνα υποβάθρου.<br /><br />Σε περίπτωση πολλών στοιχείων, θα πρέπει να διαχωρίζονται με κόμμα (π.χ. body, .myclass, #myid).";
+	public $APL_image_label = "Εικόνα υποβάθρου ιστοσελίδας";
+	public $APL_image_placeholder = " Επιλέξτε μια εικόνα υποβάθρου ";	
+	public $APL_image_desc = "<strong>ΕΙΚΟΝΑ ΥΠΟΒΑΘΡΟΥ</strong><br />--------------------------------------<br /><br />Επιλέξτε μια από τις διαθέσιμες εικόνες της λίστας για το υπόβαθρο της σελίδας σας.<br /><br />Εφόσον έχετε τα απαραίτητα δικαιώματα πρόσβασης, μπορείτε να αλλάξετε την διαδρομή που βρίσκονται οι εικόνες σας μέσα από το αρχείο params.php.";
+	public $APL_position_label = "Στοίχιση εικόνας υποβάθρου";
+	public $APL_position_placeholder = " Επιλέξτε μια στοίχιση ";
+	public $APL_position_desc = "<strong>ΣΤΟΙΧΙΣΗ ΕΙΚΟΝΑΣ</strong><br />--------------------------------------<br /><br />Επιλέξτε την οριζόντια και κάθετη στοίχιση της εικόνας υποβάθρου.<br /><br />Σας παρέχονται 9 επιλογές στοίχισης.";
+	public $APL_opt_LeftTop = "Αριστερά - Επάνω";
+	public $APL_opt_LeftCenter = "Αριστερά - Κέντρο";
+	public $APL_opt_LeftBottom = "Αριστερά - Κάτω";
+	public $APL_opt_RightTop = "Δεξιά - Επάνω";
+	public $APL_opt_RightCenter = "Δεξιά - Κέντρο";
+	public $APL_opt_RightBottom = "Δεξιά - Κάτω";
+	public $APL_opt_CenterTop = "Κέντρο - Επάνω";
+	public $APL_opt_CenterCenter = "Κέντρο - Κέντρο";
+	public $APL_opt_CenterBottom = "Κέντρο - Κάτω";
+	public $APL_attachment_label = "Επισύναψη εικόνας υποβάθρου";
+	public $APL_attachment_placeholder = " Επιλέξτε μια επισύναψη υποβάθρου";
+	public $APL_attachment_desc = "<strong>ΕΠΙΣΥΝΑΨΗ ΕΙΚΟΝΑΣ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε να επιλέξετε την μέθοδο επισύναψης της εικόνας υποβάθρου στην ιστοσελίδα σας.<br /><br />Η μέθοδος της αυτόματης διόρθωσης, θα καταστήσει ακίνητη και σταθερή την εικόνα υποβάθρου κατά την κύλιση της σελίδας. Η 2η μέθοδος θα έχει ως αποτέλεσμα την κύλιση της εικόνας υποβαθρου μαζί με το περιεχόμενο της σελίδας.";
+	public $APL_opt_Fixed = "Αυτόματη διόρθωση";
+	public $APL_opt_Scroll = "Κυλιόμενο";
+	public $APL_repeat_label = "Επανάληψη εικόνας υποβάθρου";
+	public $APL_repeat_placeholder = " Επιλέξτε μια επανάληψη εικόνας ";
+	public $APL_repeat_desc = "<strong>ΕΠΑΝΑΛΗΨΗ ΕΙΚΟΝΑΣ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε να επιλέξετε εάν η εικόνα θα επαναλαμβάνεται ή όχι και τον τρόπο της επανάληψης.<br /><br />Σας παρέχονται 4 επιλογές επανάληψης.";
+	public $APL_opt_NoRepeat = "Χωρίς επανάληψη";
+	public $APL_opt_Repeat = "Επανάληψη";
+	public $APL_opt_RepeatX = "Οριζόντια επανάληψη";
+	public $APL_opt_RepeatY = "Κάθετη επανάληψη";
+	public $APL_bgcolor_label = "Χρώμα Υποβάθρου";
+	public $APL_bgcolor_desc = "<strong>ΧΡΩΜΑ ΥΠΟΒΑΘΡΟΥ</strong><br />--------------------------------------<br /><br />Πατήστε διπλό κλικ στο διπλανό πλαίσιο εισαγωγής για να εμφανιστεί το πλαίσιο επιλογής χρώματος υποβάθρου.<br /><br />Έχετε την δυνατότητα να καταχωρήσετε και χειροκίνητα το χρώμα της αρεσκείας σας, σε μορφή RRGGBB (Προσοχή!! χωρίς το # μπροστά).";
+}
+?>
---
+++ languages/en.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ***********************************************************************************
+ * @ASCOOS-NAME        : ASCOOS CMS 24'                                            *
+ * @ASCOOS-VERSION     : 24.0.0                                                    *
+ * @ASCOOS-CATEGORY    : Block (Frontend and Administrator Side)                   *
+ * @ASCOOS-CREATOR     : Drogidis Christos                                         *
+ * @ASCOOS-SITE        : www.ascoos.com                                            *
+ * @ASCOOS-LICENSE     : [Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html  *
+ * @ASCOOS-COPYRIGHT   : Copyright (c) 2007 - 2024, AlexSoft Software.             *
+ ***********************************************************************************
+ *
+ * @package            : Background Selector
+ * @subpackage         : English Languages (en)
+ * @source             : /[BLOCKS PATH]/background_selector/languages/en.php
+ * @fileNo             : 
+ * @version            : 1.0.1
+ * @created            : 2012-07-27 20:00:00 UTC+3 
+ * @updated            : 2024-09-29 07:00:00 UTC+3
+ * @author             : Drogidis Christos
+ * @authorSite         : www.alexsoft.gr
+ * @translator	       :
+ * @translatorSite     :
+ * @translatorCreated  :
+ * @translatedUpdated  : 
+*/
+
+defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
+
+class TBlock_background_selector_Language extends TObject
+{
+	public $blockname = "Select background website";
+
+	public $APL_license = "Ascoos General License (AGL)";
+	public $APL_author = "CHRIS DROGKIDIS";
+	public $APL_creation = "";
+	public $APL_copyright = "ALEXSOFT SOFTWARE";
+	public $APL_translator = "KOURTIDIS GEORGE";
+	public $APL_translatorEmail = "";
+	public $APL_translatorUrl = "";
+	public $APL_desc = "With this implant, we can dynamically change the image of the background of our website, or even the isolated elements as DIVS, etc.";
+	
+	public $APL_paramgroup_dates_label = "▼ &nbsp; Options of Dates of Application Configurations";
+	public $APL_paramgroup_dates_label_info = "<strong>CHOICES OF DATES</strong><br />--------------------------------------<br /><br />In this context you can choose whether you want this section to have time limits of operation.";
+	public $APL_sdate_label = "Date of beginning of appearance of background";
+	public $APL_sdate_desc = "<strong>EFFECTIVE DATE</strong><br />--------------------------------------<br /><br />Click next to input box, choose the date of beginning of operation of this section.<br /><br />You can give a later date of beginning.";
+	public $APL_edate_label = "Date of expiry of background";
+	public $APL_edate_desc = "<strong>EXPIRATION DATE</strong><br />--------------------------------------<br /><br />Click next to input box, choose the date of expiry of operation of this section.<br /><br />It is recommended to use this option to have targeted presentations per page or days, etc.";
+
+	public $APL_paramgroup_bg_label = "▼ &nbsp; Background image options";
+	public $APL_paramgroup_bg_label_info = "<strong>IMAGE OPTIONS</strong><br />--------------------------------------<br /><br />In this frame you can select various parameters to customize the background image.";
+	public $APL_id_label = "CSS Selector";
+	public $APL_id_desc = "<strong>CSS SELECTORS</strong><br />--------------------------------------<br /><br />Give here the CSS item or items that you want they contain the selected picture of background.<br /><br />In the case of many elements should be separated by commas (eg, body,. Myclass, # myid).";
+	public $APL_image_label = "Website background image";
+	public $APL_image_placeholder = " Select a background image ";	
+	public $APL_image_desc = "<strong>BACKGROUND IMAGE</strong><br />--------------------------------------<br /><br />Select from the list of available images for the background of your page.<br /><br />Provided that you have the essential rights of access, you can change the root of your pictures is found, through the file params.php.";
+	public $APL_position_label = "Background image alignment";
+	public $APL_position_placeholder = " Select an alignment ";
+	public $APL_position_desc = "<strong>IMAGE POSITION</strong><br />--------------------------------------<br /><br />Select the horizontal and vertical alignment of the image background.<br /><br />You are provided with 9 alignment options.";
+	public $APL_opt_LeftTop = "Left - Top";
+	public $APL_opt_LeftCenter = "Left - Center";
+	public $APL_opt_LeftBottom = "Left - Bottom";
+	public $APL_opt_RightTop = "Right - Top";
+	public $APL_opt_RightCenter = "Right - Center";
+	public $APL_opt_RightBottom = "Right - Bottom";
+	public $APL_opt_CenterTop = "Center - Top";
+	public $APL_opt_CenterCenter = "Center - Center";
+	public $APL_opt_CenterBottom = "Center - Bottom";
+	public $APL_attachment_label = "Attach a background image";
+	public $APL_attachment_placeholder = " Select a background attachment";
+	public $APL_attachment_desc = "<strong>IMAGE ATTACHMENT</strong><br />--------------------------------------<br /><br />Here you can select the method to attach the background image on your website.<br /><br />The method of automatic correction, make real and steady the image background when scrolling the page. The second method will result in scrolling the background image, along with the page content.";
+	public $APL_opt_Fixed = "AutoCorrect";
+	public $APL_opt_Scroll = "Rolling";
+	public $APL_repeat_label = "Repeating background image";
+	public $APL_repeat_placeholder = " Select a replay image ";
+	public $APL_repeat_desc = "<strong>IMAGE REPEAT</strong><br />--------------------------------------<br /><br />Here you can choose whether the image will be repeated or not and the way of repetition.<br /><br />We provide 4 selections of repetition.";
+	public $APL_opt_NoRepeat = "Uncommitted";
+	public $APL_opt_Repeat = "Repetition";
+	public $APL_opt_RepeatX = "Horizontal repetition";
+	public $APL_opt_RepeatY = "Vertical repetition";
+	public $APL_bgcolor_label = "Background color";
+	public $APL_bgcolor_desc = "<strong>BACKGROUND COLOR</strong><br />--------------------------------------<br /><br />Double-click in the next input box to display the background color selection box.<br /><br />You can manually enter the color of your choice, in the form RRGGBB (Attention! Without the # in front).";
+}
+?>
